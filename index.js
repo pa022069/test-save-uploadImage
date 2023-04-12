90c6988b9f9725ed533636d06864b48ed1542b7e
Wistronits <pa022069@gmail.com>
2023-04-12 11:16:20 +0800

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bodyparser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const engine = require("ejs-locals");
-const uploadImage = require('./routes/uploadImage');
+
 const app = express();
 
 app.use(cors({
@@ -26,12 +26,12 @@ app.use(bodyparser.json({ limit: '3mb' }));
 
 app.use(express.static('./uploads'));
 
-app.use('/api', uploadImage);
+app.use('/api', require('./routes/uploadImage'));
 
 app.get("/share", (req, res) => {
   res.render('share', {
     title: '迪士尼百年慶典木質拼圖',
-    shareImg: `https://disney.jeffrey-work.com/uploads/${req.query.shareImg}`,
+    shareImg: `https://project100.hellofish.com.tw/uploads/${req.query.shareImg}`,
     // shareImg: `http://97.74.84.85:4001/${req.query.shareImg}`,
     description: '收錄一百位迪士尼宇宙的人氣角色，你猜得出來這些角色的名稱跟出自哪部作品嗎?'
   });
